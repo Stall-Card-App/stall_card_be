2e05740c0a614dec79fc9098173ac0ac9ea1d967
Stephanie Helm <stephaniemhelm@gmail.com>
2022-02-07 13:37:02 -0700

    mutations: add owner and farrier

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,8 @@ module Types
     field :add_barn, mutation: Mutations::AddBarn
     field :update_barn, mutation: Mutations::UpdateBarn
     field :destroy_barn, mutation: Mutations::DestroyBarn
+    field :add_vet, mutation: Mutations::AddVet
+    field :add_owner, mutation: Mutations::AddOwner
+    field :add_farrier, mutation: Mutations::AddFarrier
   end
 end
