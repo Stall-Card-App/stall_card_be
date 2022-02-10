9970c7014286341c8904611426a5a42f9aa9d069
Jacob Yarborough <jayarborough95@gmail.com>
2022-02-10 16:14:35 -0700

    Add testing for forecast endpoint

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,7 +1,7 @@
 class ForecastsController < ApplicationController
   def show 
     if params[:location] == ' ' || params[:location] == ''
-      render json: {error: "Can't find location or location was enetered incorrectly", status: 404}, status: 404
+      render json: {error: "Can't find location or location was entered incorrectly", status: 404}, status: 404
     else 
       coordinates = MapquestFacade.get_location_details(params[:location])
       forecast = OpenWeatherFacade.get_weather(coordinates[:lat], coordinates[:lng])
