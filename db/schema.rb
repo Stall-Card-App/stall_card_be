af287463717e282f415d93b935100f883565079a
Stephanie Helm <stephaniemhelm@gmail.com>
2022-02-15 13:16:43 -0700

    fix destroy specs and add photo to horse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_191706) do
+ActiveRecord::Schema.define(version: 2022_02_15_201531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_191706) do
     t.bigint "farrier_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["barn_id"], name: "index_horses_on_barn_id"
     t.index ["farrier_id"], name: "index_horses_on_farrier_id"
     t.index ["owner_id"], name: "index_horses_on_owner_id"
